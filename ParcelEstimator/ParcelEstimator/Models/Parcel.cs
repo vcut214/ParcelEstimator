dd1087042cc3aec3244a43bfdbfdd0f0e39151ca
vcut214 <26867769+vcut214@users.noreply.github.com>
2021-11-29 19:15:52 +1300

    Remove unneeded comment

--- ParcelEstimator/ParcelEstimator/Models/Parcel.cs
+++ ParcelEstimator/ParcelEstimator/Models/Parcel.cs
@@ -24,11 +24,3 @@ namespace ParcelEstimator.Models
         public double Height { get; set; }
     }
 }
-
-
-/*
- Small parcel: all dimensions < 10cm. Cost $3
-○ Medium parcel: all dimensions < 50cm. Cost $8
-○ Large parcel: all dimensions < 100cm. Cost $15
-○ XL parcel: any dimension >= 100cm. Cost $25
-*/
