afaba0e34dbaca5d98d8e3085a93ea4c0f9620dd
vcut214 <26867769+vcut214@users.noreply.github.com>
2021-11-30 20:51:41 +1300

    - Create ParcelQuotingService to generate a list of LineItems from a list of Parcels - Create helper method to create one LineItem from a Parcel

---
+++ ParcelEstimator/ParcelEstimator.Tests/ServiceTests/ParcelQuotingServiceTests.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using ParcelEstimator.Models;
+using ParcelEstimator.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelEstimator.Tests.ServiceTests
+{
+    public class ParcelQuotingServiceTests
+    {
+        [Test]
+        public void TestCreateLineItemFromParcel_SmallParcel()
+        {
+            // Arrange
+            Parcel newParcel = new Parcel()
+            {
+                Length = 9,
+                Width = 9,
+                Height = 9
+            };
+
+            // Act
+            var resultLineItem = ParcelQuotingService.CreateLineItemFromParcel(newParcel);
+
+            // Assert
+            Assert.AreEqual(3.00, resultLineItem.Cost);
+            Assert.AreEqual($"Small Parcel ({newParcel.Length}L x {newParcel.Width}W x {newParcel.Height}H)", resultLineItem.Description);
+        }
+
+        [Test]
+        public void TestCreateLineItemFromParcel_MediumParcel()
+        {
+            // Arrange
+            Parcel newParcel = new Parcel()
+            {
+                Length = 9,
+                Width = 9,
+                Height = 10
+            };
+
+            // Act
+            var resultLineItem = ParcelQuotingService.CreateLineItemFromParcel(newParcel);
+
+            // Assert
+            Assert.AreEqual(8.00, resultLineItem.Cost);
+            Assert.AreEqual($"Medium Parcel ({newParcel.Length}L x {newParcel.Width}W x {newParcel.Height}H)", resultLineItem.Description);
+        }
+
+        [Test]
+        public void TestCreateLineItemFromParcel_LargeParcel()
+        {
+            // Arrange
+            Parcel newParcel = new Parcel()
+            {
+                Length = 50,
+                Width = 49,
+                Height = 49
+            };
+
+            // Act
+            var resultLineItem = ParcelQuotingService.CreateLineItemFromParcel(newParcel);
+
+            // Assert
+            Assert.AreEqual(15.00, resultLineItem.Cost);
+            Assert.AreEqual($"Large Parcel ({newParcel.Length}L x {newParcel.Width}W x {newParcel.Height}H)", resultLineItem.Description);
+        }
+
+        [Test]
+        public void TestCreateLineItemFromParcel_XLParcel()
+        {
+            // Arrange
+            Parcel newParcel = new Parcel()
+            {
+                Length = 101,
+                Width = 100,
+                Height = 100
+            };
+
+            // Act
+            var resultLineItem = ParcelQuotingService.CreateLineItemFromParcel(newParcel);
+
+            // Assert
+            Assert.AreEqual(25.00, resultLineItem.Cost);
+            Assert.AreEqual($"XL Parcel ({newParcel.Length}L x {newParcel.Width}W x {newParcel.Height}H)", resultLineItem.Description);
+        }
+    }
+}
---
+++ ParcelEstimator/ParcelEstimator/Services/ParcelQuotingService.cs
@@ -0,0 +1,31 @@
+﻿using ParcelEstimator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelEstimator.Services
+{
+    public static class ParcelQuotingService
+    {
+        public static IEnumerable<LineItem> GetQuoteForParcels(IEnumerable<Parcel> parcels)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static LineItem CreateLineItemFromParcel(Parcel parcel)
+        {
+            double largestDimension = Math.Max(parcel.Length, Math.Max(parcel.Width, parcel.Height));
+
+            if (largestDimension < 10)
+                return new LineItem(3.00, $"Small Parcel ({parcel.Length}L x {parcel.Width}W x {parcel.Height}H)");
+            else if (largestDimension < 50)
+                return new LineItem(8.00, $"Medium Parcel ({parcel.Length}L x {parcel.Width}W x {parcel.Height}H)");
+            else if (largestDimension < 100)
+                return new LineItem(15.00, $"Large Parcel ({parcel.Length}L x {parcel.Width}W x {parcel.Height}H)");
+
+            return new LineItem(25.00, $"XL Parcel ({parcel.Length}L x {parcel.Width}W x {parcel.Height}H)");
+        }
+    }
+}
