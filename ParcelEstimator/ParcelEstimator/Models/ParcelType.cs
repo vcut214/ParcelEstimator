c7c29a8b9415c6d8d6f00ab03cc66e752626f536
vcut214 <26867769+vcut214@users.noreply.github.com>
2021-12-01 07:26:13 +1300

    Introduce ParcelType to capture related information dependent on Type (i.e. Base cost, weight limit)

---
+++ ParcelEstimator/ParcelEstimator/Models/ParcelType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcelEstimator.Models
+{
+    internal enum ParcelSize
+    {
+        Small,
+        Medium,
+        Large,
+        XL
+    }
+
+    internal class ParcelType
+    {
+        internal ParcelSize Size { get; set; }
+        internal double BaseCost { get; set; }
+        internal double WeightLimit { get; set; }
+    }
+}
